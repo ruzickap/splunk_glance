2673d03fe7034f36e2d5dc044548444417075378
Petr Ruzicka <petr.ruzicka@gmail.com>
2012-03-17 10:05:10 +0100

    Fixed rpm installation.

--- ks/splunk_installation.sh
+++ ks/splunk_installation.sh
@@ -3,15 +3,15 @@
 SPLUNK_URL="http://192.168.122.1/~pruzicka/ks/splunk-4.3.1-119532-linux-2.6-x86_64.rpm"
 SPLUNK_RPM=`basename $SPLUNK_URL`
 
-wget -P / $SPLUNK_URL
-if [ ! -f /$SPLUNK_RPM ]; then
-  echo -e "Can not find the splunk RPM in /$SPLUNK_RPM !\n"
-  echo -e "Please check if the \"$SPLUNK_RPM\" file was downloaded successfully from: $SPLUNK_URL\n"
+wget -P /tmp/ $SPLUNK_URL
+if [ ! -f /tmp/$SPLUNK_RPM ]; then
+  echo -e "Can not find the splunk RPM in /tmp/$SPLUNK_RPM !\n"
+  echo -e "Please check if the \"/tmp/$SPLUNK_RPM\" file was downloaded successfully from: $SPLUNK_URL\n"
   exit 1
 fi
 
 ## Splunk
-rpm -Uvh /$SPLUNK_RPM
+rpm -Uvh /tmp/$SPLUNK_RPM
 #Enable SSL
 #sed -i.orig 's/^enableSplunkWebSSL = false/enableSplunkWebSSL = true/' /opt/splunk/etc/system/default/web.conf
 sed -i.orig 's/^\(enableSplunkWebSSL\) = false/\1 = true/' /opt/splunk/etc/system/default/web.conf
